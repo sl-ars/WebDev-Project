9f572d3842a669900a0d0d0e6853fd33625476ed
ars-qb <arssai.main@gmail.com>
2024-04-26 01:25:45 +0500

    commit

--- frontend/src/app/register/register.component.ts
+++ frontend/src/app/register/register.component.ts
@@ -13,6 +13,10 @@ export class RegisterComponent implements OnInit {
   surname: string = '';
   email: string = '';
   password: string = '';
+  retypePassword: string = '';
+
+  showNotification: boolean = false;
+  errorMessage: string = '';
 
   constructor(private userService: UserService,
               private route: Router) { }
@@ -20,14 +24,27 @@ export class RegisterComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  showError(errorText: string){
+    this.errorMessage = errorText;
+        this.showNotification = true;
+
+        setTimeout(() => {
+          this.showNotification = false;
+        }, 3000);
+  }
   Register(){
+      if (this.password != this.retypePassword){
+        this.showError("Passwords do not match!");
+        return;
+      }
+
       this.userService.createUser(this.login, this.password, this.name, this.surname, this.email).subscribe(
         (data) => {
           alert("Success! Please log in")
           this.route.navigateByUrl('login')
         },
         err => {
-          this.userService.alert("Error occured")
+            this.showError("Error");
         }
       );
       //this.email = this.surname = this.name = this.password = this.login = "";

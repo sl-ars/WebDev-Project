a658af474faa93eb2a9e33682975d43e53da50ef
ars-qb <arssai.main@gmail.com>
2024-04-26 01:10:30 +0500

    commit

--- frontend/src/app/currencies/currencies.component.ts
+++ frontend/src/app/currencies/currencies.component.ts
@@ -68,9 +68,15 @@ export class CurrenciesComponent implements OnInit {
     if (this.last_active != "Id") this.checkLastActive();
     if (this.IdClass == "descending") {
       this.IdClass = "ascending";
+      this.currencies.sort((a, b) => {
+        return a.id - b.id;
+      });
     }
     else {
       this.IdClass = "descending";
+      this.currencies.sort((a, b) => {
+        return b.id - a.id;
+      });
     }
     this.last_active = "Id";
     console.log(this.IdClass);
@@ -80,66 +86,105 @@ export class CurrenciesComponent implements OnInit {
     if (this.last_active != "Name") this.checkLastActive();
     if (this.NameClass == "descending") {
       this.NameClass = "ascending";
+      this.currencies.sort((a, b) => {
+        return a.name.localeCompare(b.name);
+      });
     }
     else {
       this.NameClass = "descending";
+      this.currencies.sort((a, b) => {
+        return b.name.localeCompare(a.name);
+      });
     }
     this.last_active = "Name";
+
+
+
   }
 
-  // sortByPrice() {
-  //   if (this.last_active != "Price") this.checkLastActive();
-  //   if (this.PriceClass == "descending") {
-  //     this.PriceClass = "ascending";
-  //   }
-  //   else {
-  //     this.PriceClass = "descending";
-  //   }
-  //   this.last_active = "Price";
-  // }
+  sortByPrice() {
+    if (this.last_active != "Price") this.checkLastActive();
+    if (this.PriceClass == "descending") {
+      this.PriceClass = "ascending";
+      this.currencies.sort((a, b) => {
+        return a.quote.USD.price - b.quote.USD.price;
+      });
+    }
+    else {
+      this.PriceClass = "descending";
+      this.currencies.sort((a, b) => {
+        return b.quote.USD.price - a.quote.USD.price;
+      });
+    }
+    this.last_active = "Price";
+  }
 
-  // sortBy24h() {
-  //   if (this.last_active != "24h") this.checkLastActive();
-  //   if (this.Class24h == "descending") {
-  //     this.Class24h = "ascending";
-  //   }
-  //   else {
-  //     this.Class24h = "descending";
-  //   }
-  //   this.last_active = "24h";
-  // }
+  sortBy24h() {
+    if (this.last_active != "24h") this.checkLastActive();
+    if (this.Class24h == "descending") {
+      this.Class24h = "ascending";
+      this.currencies.sort((a, b) => {
+        return a.quote.USD.percent_change_24h - b.quote.USD.percent_change_24h;
+      });
+    }
+    else {
+      this.Class24h = "descending";
+      this.currencies.sort((a, b) => {
+        return b.quote.USD.percent_change_24h - a.quote.USD.percent_change_24h;
+      });
+    }
+    this.last_active = "24h";
+  }
 
-  // sortBy7d() {
-  //   if (this.last_active != "7d") this.checkLastActive();
-  //   if (this.Class7d == "descending") {
-  //     this.Class7d = "ascending";
-  //   }
-  //   else {
-  //     this.Class7d = "descending";
-  //   }
-  //   this.last_active = "7d";
-  // }
+  sortBy7d() {
+    if (this.last_active != "7d") this.checkLastActive();
+    if (this.Class7d == "descending") {
+      this.Class7d = "ascending";
+      this.currencies.sort((a, b) => {
+        return a.quote.USD.percent_change_7d - b.quote.USD.percent_change_7d;
+      });
+    }
+    else {
+      this.Class7d = "descending";
+      this.currencies.sort((a, b) => {
+        return b.quote.USD.percent_change_7d - a.quote.USD.percent_change_7d;
+      });
+    }
+    this.last_active = "7d";
+  }
 
-  // sortByMarketCap() {
-  //   if (this.last_active != "MarketCap") this.checkLastActive();
-  //   if (this.MarketCapClass == "descending") {
-  //     this.MarketCapClass = "ascending";
-  //   }
-  //   else {
-  //     this.MarketCapClass = "descending";
-  //   }
-  //   this.last_active = "MarketCap";
-  // }
+  sortByMarketCap() {
+    if (this.last_active != "MarketCap") this.checkLastActive();
+    if (this.MarketCapClass == "descending") {
+      this.MarketCapClass = "ascending";
+      this.currencies.sort((a, b) => {
+        return a.quote.USD.market_cap - b.quote.USD.market_cap;
+      });
+    }
+    else {
+      this.MarketCapClass = "descending";
+      this.currencies.sort((a, b) => {
+        return b.quote.USD.market_cap - a.quote.USD.market_cap;
+      });
+    }
+    this.last_active = "MarketCap";
+  }
 
-  // sortByVolume() {
-  //   if (this.last_active != "Volume") this.checkLastActive();
-  //   if (this.VolumeClass == "descending") {
-  //     this.VolumeClass = "ascending";
-  //   }
-  //   else {
-  //     this.VolumeClass = "descending";
-  //   }
-  //   this.last_active = "Volume";
-  // }
+  sortByVolume() {
+    if (this.last_active != "Volume") this.checkLastActive();
+    if (this.VolumeClass == "descending") {
+      this.VolumeClass = "ascending";
+      this.currencies.sort((a, b) => {
+        return a.quote.USD.volume_24h - b.quote.USD.volume_24h;
+      });
+    }
+    else {
+      this.VolumeClass = "descending";
+      this.currencies.sort((a, b) => {
+        return b.quote.USD.volume_24h - a.quote.USD.volume_24h;
+      });
+    }
+    this.last_active = "Volume";
+  }
 
 }
--- frontend/src/models.ts
+++ frontend/src/models.ts
@@ -1,5 +1,5 @@
 export interface Message {
-  message: String
+  message: string
 }
 
 export interface PortfolioElement {
@@ -13,7 +13,7 @@ export interface PortfolioElement {
 
 export interface Transaction {
   id: number,
-  type: String,
+  type: string,
   user_id: number,
   currency_id: number,
   quantity: number,
@@ -57,8 +57,8 @@ export interface Portfolio {
 
 export interface Currency {
     id: number,
-    name: String,
-    symbol: String,
+    name: string,
+    symbol: string,
     cmc_rank: number,
     circulating_supply: number,
     max_supply: number,
@@ -76,49 +76,14 @@ export interface Currency {
 
 export interface CurrencyMetadata {
     id: number,
-    description: String,
-    logo: String,
+    description: string,
+    logo: string,
     urls: {
-        website: String[],
-        twitter: String[],
-        facebook: String[],
-        reddit: String[],
-        technical_doc: String[],
-        source_code: String[]
+        website: string[],
+        twitter: string[],
+        facebook: string[],
+        reddit: string[],
+        technical_doc: string[],
+        source_code: string[]
     }
 }
-
-// export const currency_metadata_example: CurrencyMetadata = {
-//     id: 1,
-//     description: 'Битко́йн, или битко́ин — пиринговая платёжная система, использующая одноимённую единицу для учёта операций. Для обеспечения функционирования и защиты системы используются криптографические методы, но при этом вся информация о транзакциях между адресами системы доступна в открытом виде.',
-//     logo: 'https://upload.wikimedia.org/wikipedia/commons/d/d2/Bitcoin_Digital_Currency_Logo.png',
-//     urls: {
-//         website: ['https://www.wikipedia.org/'],
-//         twitter: ['https://www.wikipedia.org/'],
-//         facebook: ['https://www.wikipedia.org/', 'https://www.wikipedia.org/'],
-//         reddit: [],
-//         technical_doc: ['https://www.wikipedia.org/'],
-//         source_code: []
-//     }
-// }
-
-// export const currencies_example: Currency[] = [
-//     {id: 1, max_supply: 200000000, cmc_rank: 1, name: 'Bitcoin', symbol: 'BTC', circulating_supply: 1929919291929, quote: {USD: {fully_diluted_market_cap: 1212121211, price: 41632.147753521356, market_cap: 791738578891.785, volume_24h: 28838242204.710552, percent_change_24h: 0.71761542, percent_change_7d: 0.78095225}}},
-//     {id: 2, max_supply: 200000000, cmc_rank: 1, name: 'Bitcoin', symbol: 'BTC', circulating_supply: 1929919291929, quote: {USD: {fully_diluted_market_cap: 1212121211, price: 41632.147753521356, market_cap: 791738578891.785, volume_24h: 28838242204.710552, percent_change_24h: 0.71761542, percent_change_7d: 0.78095225}}},
-//     {id: 3, max_supply: 200000000, cmc_rank: 1, name: 'Bitcoin', symbol: 'BTC', circulating_supply: 1929919291929, quote: {USD: {fully_diluted_market_cap: 1212121211, price: 41632.147753521356, market_cap: 791738578891.785, volume_24h: 28838242204.710552, percent_change_24h: 0.71761542, percent_change_7d: 0.78095225}}},
-//     {id: 4, max_supply: 200000000, cmc_rank: 1, name: 'Bitcoin', symbol: 'BTC', circulating_supply: 1929919291929, quote: {USD: {fully_diluted_market_cap: 1212121211, price: 41632.147753521356, market_cap: 791738578891.785, volume_24h: 28838242204.710552, percent_change_24h: 0.71761542, percent_change_7d: 0.78095225}}},
-//     {id: 5, max_supply: 200000000, cmc_rank: 1, name: 'Bitcoin', symbol: 'BTC', circulating_supply: 1929919291929, quote: {USD: {fully_diluted_market_cap: 1212121211, price: 41632.147753521356, market_cap: 791738578891.785, volume_24h: 28838242204.710552, percent_change_24h: 0.71761542, percent_change_7d: 0.78095225}}},
-//     {id: 6, max_supply: 200000000, cmc_rank: 1, name: 'Bitcoin', symbol: 'BTC', circulating_supply: 1929919291929, quote: {USD: {fully_diluted_market_cap: 1212121211, price: 41632.147753521356, market_cap: 791738578891.785, volume_24h: 28838242204.710552, percent_change_24h: 0.71761542, percent_change_7d: 0.78095225}}},
-//     {id: 7, max_supply: 200000000, cmc_rank: 1, name: 'Bitcoin', symbol: 'BTC', circulating_supply: 1929919291929, quote: {USD: {fully_diluted_market_cap: 1212121211, price: 41632.147753521356, market_cap: 791738578891.785, volume_24h: 28838242204.710552, percent_change_24h: 0.71761542, percent_change_7d: 0.78095225}}},
-//     {id: 8, max_supply: 200000000, cmc_rank: 1, name: 'Bitcoin', symbol: 'BTC', circulating_supply: 1929919291929, quote: {USD: {fully_diluted_market_cap: 1212121211, price: 41632.147753521356, market_cap: 791738578891.785, volume_24h: 28838242204.710552, percent_change_24h: 0.71761542, percent_change_7d: 0.78095225}}},
-//     {id: 9, max_supply: 200000000, cmc_rank: 1, name: 'Bitcoin', symbol: 'BTC', circulating_supply: 1929919291929, quote: {USD: {fully_diluted_market_cap: 1212121211, price: 41632.147753521356, market_cap: 791738578891.785, volume_24h: 28838242204.710552, percent_change_24h: 0.71761542, percent_change_7d: 0.78095225}}},
-//     {id: 10, max_supply: 200000000, cmc_rank: 1, name: 'Bitcoin', symbol: 'BTC', circulating_supply: 1929919291929, quote: {USD: {fully_diluted_market_cap: 1212121211, price: 41632.147753521356, market_cap: 791738578891.785, volume_24h: 28838242204.710552, percent_change_24h: 0.71761542, percent_change_7d: 0.78095225}}},
-//     {id: 11, max_supply: 200000000, cmc_rank: 1, name: 'Bitcoin', symbol: 'BTC', circulating_supply: 1929919291929, quote: {USD: {fully_diluted_market_cap: 1212121211, price: 41632.147753521356, market_cap: 791738578891.785, volume_24h: 28838242204.710552, percent_change_24h: 0.71761542, percent_change_7d: 0.78095225}}},
-//     {id: 12, max_supply: 200000000, cmc_rank: 1, name: 'Bitcoin', symbol: 'BTC', circulating_supply: 1929919291929, quote: {USD: {fully_diluted_market_cap: 1212121211, price: 41632.147753521356, market_cap: 791738578891.785, volume_24h: 28838242204.710552, percent_change_24h: 0.71761542, percent_change_7d: 0.78095225}}},
-//     {id: 13, max_supply: 200000000, cmc_rank: 1, name: 'Bitcoin', symbol: 'BTC', circulating_supply: 1929919291929, quote: {USD: {fully_diluted_market_cap: 1212121211, price: 41632.147753521356, market_cap: 791738578891.785, volume_24h: 28838242204.710552, percent_change_24h: 0.71761542, percent_change_7d: 0.78095225}}},
-//     {id: 14, max_supply: 200000000, cmc_rank: 1, name: 'Bitcoin', symbol: 'BTC', circulating_supply: 1929919291929, quote: {USD: {fully_diluted_market_cap: 1212121211, price: 41632.147753521356, market_cap: 791738578891.785, volume_24h: 28838242204.710552, percent_change_24h: 0.71761542, percent_change_7d: 0.78095225}}},
-//     {id: 15, max_supply: 200000000, cmc_rank: 1, name: 'Bitcoin', symbol: 'BTC', circulating_supply: 1929919291929, quote: {USD: {fully_diluted_market_cap: 1212121211, price: 41632.147753521356, market_cap: 791738578891.785, volume_24h: 28838242204.710552, percent_change_24h: 0.71761542, percent_change_7d: 0.78095225}}},
-//     {id: 16, max_supply: 200000000, cmc_rank: 1, name: 'Bitcoin', symbol: 'BTC', circulating_supply: 1929919291929, quote: {USD: {fully_diluted_market_cap: 1212121211, price: 41632.147753521356, market_cap: 791738578891.785, volume_24h: 28838242204.710552, percent_change_24h: 0.71761542, percent_change_7d: 0.78095225}}},
-//     {id: 17, max_supply: 200000000, cmc_rank: 1, name: 'Bitcoin', symbol: 'BTC', circulating_supply: 1929919291929, quote: {USD: {fully_diluted_market_cap: 1212121211, price: 41632.147753521356, market_cap: 791738578891.785, volume_24h: 28838242204.710552, percent_change_24h: 0.71761542, percent_change_7d: 0.78095225}}},
-//     {id: 18, max_supply: 200000000, cmc_rank: 1, name: 'Bitcoin', symbol: 'BTC', circulating_supply: 1929919291929, quote: {USD: {fully_diluted_market_cap: 1212121211, price: 41632.147753521356, market_cap: 791738578891.785, volume_24h: 28838242204.710552, percent_change_24h: 0.71761542, percent_change_7d: 0.78095225}}},
-// ]
